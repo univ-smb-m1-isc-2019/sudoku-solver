9bff1003c774fea3ede4814c8af8c17baff204aa
Nerah <alascenci@gmail.com>
2020-03-02 18:50:28 +0100

    Initial project structure.

---
+++ src/main/java/sudoku_solver/SudokuSolver.kt
@@ -0,0 +1,5 @@
+package sudoku_solver
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ src/test/java/sudoku_solver/SudokuSolverTest.kt
@@ -0,0 +1,11 @@
+package sudoku_solver
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class SudokuSolverTest {
+    @Test
+    fun shouldAlwaysReturnTrue() {
+        assertTrue { true }
+    }
+}
