7579546c50706bf78b98346ccb6099f3e9d9c20e
Nerah <alascenci@gmail.com>
2020-03-06 17:13:15 +0100

    GridTranscriber fonctionne dans les deux sens.

--- src/main/java/sudoku_solver/GridTranscriber.kt
+++ src/main/java/sudoku_solver/GridTranscriber.kt
@@ -5,5 +5,8 @@ class GridTranscriber() {
         fun translate(originGrid: Array<Array<Int>>): List<List<Square>> {
             return originGrid.map { it -> it.map { Square(it) } }
         }
+        fun translate(originGrid: List<List<Square>>): Array<Array<Int>> {
+            return originGrid.map { it -> it.map { it.value }.toTypedArray() }.toTypedArray()
+        }
     }
 }
