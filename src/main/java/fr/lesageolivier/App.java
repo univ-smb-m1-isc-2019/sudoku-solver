6d7beec03857046e6b0eed853a785023c65bf821
Olivier LESAGE <olivier.lesage@etu.univ-savoie.fr>
2020-03-09 18:02:16 +0100

    Création du projet Maven.

---
+++ src/main/java/fr/lesageolivier/App.java
@@ -0,0 +1,13 @@
+package fr.lesageolivier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
