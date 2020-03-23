a5f7f47a9515099b2b1b10b4392eb1052bd7f19a
titi73370 <thib.chami@gmail.com>
2020-03-23 19:04:51 +0100

    ajout de la fonction resoudre

--- SudokuSolver/src/main/java/Sudoku.java
+++ SudokuSolver/src/main/java/Sudoku.java
@@ -1,25 +1,49 @@
 public class Sudoku {
-    public int[][] SudokuGrid = new int[9][9];
+    private boolean resolu = false;
 
-    public Sudoku(int[][] grid) {
+    public Sudoku(int[][] board) {
+        afficherGrille(board);
+        resoudre(board, 0, 0);
+    }
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                this.SudokuGrid[i][j] = grid[i][j];
+    public void resoudre(int[][] g, int a, int b) {
+        if (b == 9) {
+            b = 0;
+            a++;
+        }
+        if (a == 9 && b == 0) {
+            if (test_grille(g)) {
+                resolu = true;
+                afficherGrille(g);
+            }
+        }
+        //
+        else {
+
+
+            if (g[a][b] == 0) {
+            }
+            else {
+                resoudre(g, a, b + 1);
             }
         }
     }
+    
+    public void afficherGrille(int[][] g) {
+        if (resolu == false) {
+            System.out.println("Sudoku à résoudre:");
+        } else {
+            System.out.println("Sudoku résolu:");
+        }
 
-    public void showGrid() {
-        System.out.println("Sudoku à résoudre:");
-        for (int i = 0; i < this.SudokuGrid.length; i++) {
+        for (int i = 0; i < g.length; i++) {
             if (i % 3 == 0)
                 System.out.println(" -------------------------");
-            for (int j = 0; j < this.SudokuGrid[i].length; j++) {
+            for (int j = 0; j < g[i].length; j++) {
                 if (j % 3 == 0) System.out.print("| ");
-                if (this.SudokuGrid[i][j] != 0){
-                    System.out.print(this.SudokuGrid[i][j] + " ");
-                }else{
+                if (g[i][j] != 0) {
+                    System.out.print(g[i][j] + " ");
+                } else {
                     System.out.print("  ");
                 }
 
@@ -28,4 +52,6 @@ public class Sudoku {
         }
         System.out.println(" -------------------------");
     }
+
+
 }
