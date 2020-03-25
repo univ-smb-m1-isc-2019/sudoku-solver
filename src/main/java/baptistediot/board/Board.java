47c0b53801e582343db7fe0e4a1829afaca6c2ce
Baptiste <baptiste.diot@hotmail.fr>
2020-03-25 15:09:07 +0100

    Added private function in Board class to create the rows, the columns and the squares

--- src/main/java/baptistediot/board/Board.java
+++ src/main/java/baptistediot/board/Board.java
@@ -1,5 +1,7 @@
 package baptistediot.board;
 
+import baptistediot.Sudoku;
+
 public class Board {
     public Cell[][] board;
     public Row[] rows;
@@ -8,10 +10,38 @@ public class Board {
 
 
     public Board(int[][] board) {
+        createEmptyRows();
+        createEmptyColumns();
+        createEmptySquare();
+
         buildBoard(board);
     }
 
     private void buildBoard(int[][] board){
 
+    }
+
+    private void createEmptyRows(){
+        this.rows = new Row[Sudoku.ROW_SIZE];
+
+        for(int row = 0; row < Sudoku.ROW_SIZE; ++row){
+            rows[row] = new Row();
+        }
+    }
+
+    private void createEmptyColumns(){
+        this.columns = new Column[Sudoku.COLUMN_SIZE];
+
+        for(int column = 0; column < Sudoku.COLUMN_SIZE; ++column){
+            columns[column] = new Column();
+        }
+    }
+
+    private void createEmptySquare(){
+        this.squares = new Square[Sudoku.SQUARE_SIZE];
+
+        for(int square = 0; square < Sudoku.SQUARE_SIZE; ++square){
+            squares[square] = new Square();
+        }
     }
 }
