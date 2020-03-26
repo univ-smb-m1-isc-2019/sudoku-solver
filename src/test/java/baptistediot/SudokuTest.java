9cb8af01ded8a119b93550dfbd9edb2eeabdc41e
Baptiste <baptiste.diot@hotmail.fr>
2020-03-26 11:41:55 +0100

    Added method's body for the column test. Added a function to get all nine values in an ArrayList of cells

--- src/test/java/baptistediot/SudokuTest.java
+++ src/test/java/baptistediot/SudokuTest.java
@@ -1,6 +1,12 @@
 package baptistediot;
 
+import baptistediot.board.Cell;
+import baptistediot.board.Column;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -8,6 +14,33 @@ public class SudokuTest {
 
     @Test
     public void columnShouldHaveEveryNumberFromOneToNineOnce() {
+        int[][] board = App.board;
+        Sudoku sudoku = new Sudoku(board);
+
+        sudoku.solve();
+
+        Integer[] solution = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for(int column = 0; column < Sudoku.COLUMN_SIZE; column++){
+            Column c = sudoku.getBoard().getColumns(column);
+
+            ArrayList<Cell> cells = c.getColumn();
+
+            Integer[] valueCells = getAllValueNineCells(cells);
+            Arrays.sort(valueCells);
+
+            assertThat(valueCells).isEqualTo(solution);
+        }
+    }
+
+    private Integer[] getAllValueNineCells(ArrayList<Cell> cells){
+        Integer[] res = new Integer[9];
+
+        for(int i = 0; i < res.length; ++i){
+            res[i] = cells.get(i).getValue();
+        }
+
+        return res;
     }
 
     @Test
