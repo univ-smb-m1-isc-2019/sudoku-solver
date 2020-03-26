bc45432451407e3dca06c5c1571c7d44a3b65758
Baptiste <baptiste.diot@hotmail.fr>
2020-03-26 11:38:15 +0100

    Added SudokuTest with function's signature to test the value of each cells in the Columns, Rows and Square

---
+++ src/test/java/baptistediot/SudokuTest.java
@@ -0,0 +1,22 @@
+package baptistediot;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SudokuTest {
+
+    @Test
+    public void columnShouldHaveEveryNumberFromOneToNineOnce() {
+    }
+
+    @Test
+    public void rowShouldHaveEveryNumberFromOneToNineOnce() {
+    }
+
+    @Test
+    public void squareShouldHaveEveryNumberFromOneToNineOnce() {
+    }
+
+
+}
