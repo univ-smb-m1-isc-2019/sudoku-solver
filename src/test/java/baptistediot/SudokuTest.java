78bdd2952571e41edf776ee9ed463a5b42b896e1
Baptiste <baptiste.diot@hotmail.fr>
2020-03-26 11:48:13 +0100

    Added method's body for the square test

--- src/test/java/baptistediot/SudokuTest.java
+++ src/test/java/baptistediot/SudokuTest.java
@@ -3,6 +3,7 @@ package baptistediot;
 import baptistediot.board.Cell;
 import baptistediot.board.Column;
 import baptistediot.board.Row;
+import baptistediot.board.Square;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ public class SudokuTest {
         Integer[] solution = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for(int column = 0; column < Sudoku.COLUMN_SIZE; column++){
-            Column c = sudoku.getBoard().getColumns(column);
+            Column c = sudoku.getBoard().getColumn(column);
 
             ArrayList<Cell> cells = c.getColumn();
 
@@ -44,7 +45,7 @@ public class SudokuTest {
         Integer[] solution = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for(int row = 0; row < Sudoku.ROW_SIZE; row++){
-            Row r = sudoku.getBoard().getRows(row);
+            Row r = sudoku.getBoard().getRow(row);
 
             ArrayList<Cell> cells = r.getRow();
 
@@ -57,6 +58,23 @@ public class SudokuTest {
 
     @Test
     public void squareShouldHaveEveryNumberFromOneToNineOnce() {
+        int[][] board = App.board;
+        Sudoku sudoku = new Sudoku(board);
+
+        sudoku.solve();
+
+        Integer[] solution = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for(int square = 0; square < Sudoku.SQUARE_SIZE; square++){
+            Square s = sudoku.getBoard().getSquare(square);
+
+            ArrayList<Cell> cells = s.getSquare();
+
+            Integer[] valueCells = getAllValueNineCells(cells);
+            Arrays.sort(valueCells);
+
+            assertThat(valueCells).isEqualTo(solution);
+        }
     }
 
     private Integer[] getAllValueNineCells(ArrayList<Cell> cells){
