cc6e52557f504957ebcc5c2f11233555296e7b78
titi73370 <thib.chami@gmail.com>
2020-03-26 23:46:59 +0100

    ajout methode numerosPossibles

--- SudokuSolver/src/main/java/Sudoku.java
+++ SudokuSolver/src/main/java/Sudoku.java
@@ -11,27 +11,57 @@ public class Sudoku {
             col = 0;
             line++;
         }
-
         if (line == 9 && col == 0) {
-            if (testSudoku(board)) {
+            if (sudokuFini(board)) {
                 resolu = true;
                 afficherSudoku(board);
             }
-        }
-
-        else {
-
+        } else {
             if (board[line][col] == 0) {
 
-            }
-
-            else {
+            } else {
                 resoudre(board, line, col + 1);
             }
         }
     }
 
-    private boolean testSudoku(int[][] board) {
+    public boolean[] numerosPossibles(int[][] g, int x, int y) {
+        boolean[] t = new boolean[10];
+        int i, j;
+
+        for (i = 1; i < 10; i++) {
+            t[i] = true;
+        }
+
+
+        for (i = 0; i < 9; i++) {
+            // met a faux dans le tableau t, à la case qui correspond à la valeur de g[i][y] si le chiffre est sur la colonne
+            if (g[i][y] != 0) {
+                if (!t[0]) t[0] = true;
+                t[g[i][y]] = false;
+            }
+            //met a faux dans le tableau t, à la case qui correspond à la valeur de g[i][y] si le chiffre est sur la ligne
+            if (g[x][i] != 0) {
+                if (!t[0]) t[0] = true;
+                t[g[x][i]] = false;
+            }
+        }
+
+        // met a faux dans le tableau t, à la case qui correspond à la valeur de g[i][y] si le chiffre est présent dans le bloc
+        int v = (x / 3) * 3;
+        int h = (y / 3) * 3;
+        for (i = v; i < v + 3; i++) {
+            for (j = h; j < h + 3; j++) {
+                if (g[i][j] != 0) {
+                    if (!t[0]) t[0] = true;
+                    t[g[i][j]] = false;
+                }
+            }
+        }
+        return t;
+    }
+
+    private boolean sudokuFini(int[][] board) {
         for (int i = 0; i < 9; i++) {
             if (calculLigne(board, i) != 45) return false;
             if (calculColonne(board, i) != 45) return false;
@@ -46,6 +76,7 @@ public class Sudoku {
         }
         return res;
     }
+
     public int calculColonne(int[][] board, int j) {
         int res = 0;
         for (int i = 0; i < 9; i++) {
@@ -54,14 +85,12 @@ public class Sudoku {
         return res;
     }
 
-
     public void afficherSudoku(int[][] board) {
         if (resolu == false) {
             System.out.println("Sudoku à résoudre:");
         } else {
             System.out.println("Sudoku résolu:");
         }
-
         for (int i = 0; i < board.length; i++) {
             if (i % 3 == 0)
                 System.out.println(" -------------------------");
