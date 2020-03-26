b34be7b3c670fe1f77ab161d797869089bb698a8
Baptiste <baptiste.diot@hotmail.fr>
2020-03-26 11:49:43 +0100

    Added method's body to solve the sudoku

--- src/main/java/baptistediot/Sudoku.java
+++ src/main/java/baptistediot/Sudoku.java
@@ -3,8 +3,6 @@ package baptistediot;
 import baptistediot.board.Board;
 import baptistediot.board.Cell;
 
-import java.util.Arrays;
-
 public class Sudoku {
     public static final int ROW_SIZE = 9;
     public static final int COLUMN_SIZE = 9;
@@ -16,7 +14,32 @@ public class Sudoku {
         this.board = new Board(board);
     }
 
-    public void solve(){
+    public boolean solve(){
+        for(int row = 0; row < ROW_SIZE; ++row) {
+            for (int column = 0; column < COLUMN_SIZE; ++column) {
+                Cell cell = board.getCell(row, column);
+
+                if(cell.isEmpty()) {
+
+                    Integer[] possibilities = CellSolver.getPossibilities(board, row, column);
+
+                    for (Integer possibility : possibilities) {
+                        cell.setValue(possibility);
+
+                        if (solve()) {
+                            return true;
+                        }
+                        else{
+                            cell.setValue(0);
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public String toString(){
