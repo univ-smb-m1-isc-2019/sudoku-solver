014d139c2b5ec3928cc768b47414688c2431be5f
Baptiste <baptiste.diot@hotmail.fr>
2020-03-26 11:40:22 +0100

    Added getter in Sudoku class

--- src/main/java/baptistediot/Sudoku.java
+++ src/main/java/baptistediot/Sudoku.java
@@ -1,23 +1,29 @@
 package baptistediot;
 
 import baptistediot.board.Board;
+import baptistediot.board.Cell;
+
+import java.util.Arrays;
 
 public class Sudoku {
     public static final int ROW_SIZE = 9;
     public static final int COLUMN_SIZE = 9;
     public static final int SQUARE_SIZE = 9;
 
-    private Board sudoku;
+    private Board board;
 
     public Sudoku(int[][] board){
-        this.sudoku = new Board(board);
+        this.board = new Board(board);
     }
 
     public void solve(){
-
     }
 
     public String toString(){
-        return sudoku.toString();
+        return board.toString();
+    }
+
+    public Board getBoard(){
+        return board;
     }
 }
