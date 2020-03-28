0467bcfacfd26ef962d361b2edf52e17e645b73a
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-03-28 17:39:19 +0100

    Ajout d'un test sur les possibilitees dans une grille non vide

--- src/test/java/sudokuTests/SudokuTests.java
+++ src/test/java/sudokuTests/SudokuTests.java
@@ -2,6 +2,8 @@ package sudokuTests;
 
 import sudokuSolver.Grille;
 import org.junit.*;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SudokuTests {
@@ -45,4 +47,29 @@ public class SudokuTests {
             }
         }
     }
+
+
+    @Test
+    public void une_case_avec_un_chiffre_n_a_plus_de_possibilitees()
+    {
+        Grille grille = new Grille();
+        grille.initGrille(9,9);
+        grille.initGrilleJeu1();
+
+        for(int i = 0; i < grille.nbLignes(); i++)
+        {
+            for(int j = 0; j < grille.nbColonnes(); j++)
+            {
+                if(grille.at(i,j).getValeur() == 0) {
+                    assertTrue(grille.at(i, j).possibleValide(9));
+                    assertTrue(grille.at(i, j).encorePossible());
+                }else
+                {
+                    assertTrue(grille.at(i, j).possibleValide(0));
+                    assertFalse(grille.at(i, j).encorePossible());
+                }
+            }
+        }
+    }
+
 }
