838c5735aba9e055bd7db2791253010b641ed72e
YannDubost <35998187+YannDubost@users.noreply.github.com>
2020-03-28 15:11:28 +0100

    Ajout de l'attribut grille dans la classe grille

--- src/main/java/sudokuSolver/Grille.java
+++ src/main/java/sudokuSolver/Grille.java
@@ -2,8 +2,22 @@ package sudokuSolver;
 
 public class Grille {
 
+    private int[][] grille;
+
     public Grille()
     {
 
+    }
+
+    public int[][] getGrille() {
+        return grille;
+    }
+
+    public void setGrille(int[][] grille) {
+        this.grille = grille;
+    }
+
+    public void setGrille(int c,int l) {
+        grille = new int[l][c];
     }
 }
