d8b2d6983692b3f1f2004c05a9257d77e38aac70
titi73370 <thib.chami@gmail.com>
2020-03-29 17:35:15 +0200

    ajout du cinquième test

--- SudokuSolver/src/test/java/TestSudoku.java
+++ SudokuSolver/src/test/java/TestSudoku.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestSudoku {
     int[][] board = {
@@ -14,6 +13,16 @@ public class TestSudoku {
             {0, 0, 1, 0, 0, 0, 0, 6, 8},
             {0, 0, 8, 5, 0, 0, 0, 1, 0},
             {0, 9, 0, 0, 0, 0, 4, 0, 0}
+    }, fausse_board = {
+        { 8, 0, 0, 0, 0, 0, 8, 0, 0 },
+        { 0, 0, 3, 6, 0, 0, 0, 0, 9 },
+        { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+        { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+        { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+        { 8, 0, 1, 0, 0, 0, 0, 6, 8 },
+        { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+        { 0, 9, 0, 0, 0, 0, 4, 0, 1 }
     };
 
     @Test
@@ -57,6 +66,15 @@ public class TestSudoku {
         for (int i = 0; i<20; i++) {
             sudok.resoudre(0,0);
             assertTrue(sudok.resolutionCorrecte());
+        }
+    }
+
+    @Test
+    public void solve_should_return_false_if_unsolvable(){
+        Sudoku sudok = new Sudoku(fausse_board);
+        for (int i = 0; i<20; i++) {
+            sudok.resoudre(0,0);
+            assertFalse(sudok.resolutionCorrecte());
         }
     }
 }
