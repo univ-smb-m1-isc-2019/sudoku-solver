514874c08de679de42059aaa644c67b33b9fee55
Tiba <baptiste-ft-du-73@hotmail.fr>
2020-03-31 18:52:50 +0200

    Creation d'une classe Cell

---
+++ src/main/java/usmb/info806/sudoku/Cell.java
@@ -0,0 +1,13 @@
+package usmb.info806.sudoku;
+
+public class Cell {
+    public int value;
+    public int x;
+    public  int y;
+
+    public Cell(int value, int x, int y){
+        this.value = value;
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/usmb/info806/sudoku/Sudoku.java
+++ src/main/java/usmb/info806/sudoku/Sudoku.java
@@ -1,8 +1,16 @@
 package usmb.info806.sudoku;
 
+import java.util.ArrayList;
+
 public class Sudoku {
+    public ArrayList<Cell> cellList;
 
     public Sudoku(int[][] board){
-
+        cellList = new ArrayList<Cell>();
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                cellList.add(new Cell(board[i][j], i, j));
+            }
+        }
     }
 }
