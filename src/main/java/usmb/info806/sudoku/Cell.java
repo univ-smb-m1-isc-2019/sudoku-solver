8fe916bdc7476ea0c37dcad462be3a89a14d708b
Tiba <baptiste-ft-du-73@hotmail.fr>
2020-04-01 18:39:58 +0200

    Initialisation des carré

--- src/main/java/usmb/info806/sudoku/Cell.java
+++ src/main/java/usmb/info806/sudoku/Cell.java
@@ -3,11 +3,17 @@ package usmb.info806.sudoku;
 public class Cell {
     public int value;
     public int x;
-    public  int y;
+    public int y;
 
     public Cell(int value, int x, int y){
         this.value = value;
         this.x = x;
         this.y = y;
     }
+
+    public int getSquareID(){
+        return  (x/3 *3) + (y/3);
+    }
+
+
 }
--- src/main/java/usmb/info806/sudoku/Sudoku.java
+++ src/main/java/usmb/info806/sudoku/Sudoku.java
@@ -9,56 +9,49 @@ public class Sudoku {
     public ArrayList<Column> columnsList;
     public ArrayList<Square> squareList;
 
-    public Sudoku(int[][] board){
+    public Sudoku(int[][] board) {
         cellList = new ArrayList<Cell>();
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[i].length; j++){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
                 cellList.add(new Cell(board[i][j], i, j));
             }
         }
         initAll();
     }
 
-    public void initLine(int nb){
+    public void initLine(int nb) {
         lineList = new ArrayList<Line>();
-        for (int i = 0; i < nb ; i++){
+        for (int i = 0; i < nb; i++) {
             lineList.add(new Line(i));
         }
 
     }
 
-    public void initColumn(int nb){
+    public void initColumn(int nb) {
         columnsList = new ArrayList<Column>();
-        for (int i = 0; i < nb ; i++){
+        for (int i = 0; i < nb; i++) {
             columnsList.add(new Column(i));
         }
 
     }
 
-    public void initSquare(int nb){
+    public void initSquare(int nb) {
         squareList = new ArrayList<Square>();
-        for (int i = 0; i < nb ; i++){
+        for (int i = 0; i < nb; i++) {
             squareList.add(new Square(i));
         }
 
     }
 
-    public void initAll(){
+    public void initAll() {
         initLine(9);
         initColumn(9);
-        //initSquare(9);
-        for (Cell cell: cellList) {
-            // On test si la case apartient a la ligne
-            if (cell.x == lineList.get(cell.x).id){
-                lineList.get(cell.x).cellList.add(cell);
-            }
-            // On test si la case apartient a la colonne
-            if (cell.y == columnsList.get(cell.y).id){
-                columnsList.get(cell.y).cellList.add(cell);
-            }
+        initSquare(9);
+        for (Cell cell : cellList) {
+            lineList.get(cell.x).cellList.add(cell);
+            columnsList.get(cell.y).cellList.add(cell);
+            squareList.get(cell.getSquareID()).cellList.add(cell);
         }
-
-
 
     }
 
