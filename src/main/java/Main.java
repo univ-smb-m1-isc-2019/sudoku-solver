0a422ffd8747599dd204b209d5cee0a7e061e66b
MaximeVigny <maxvigny@orange.fr>
2020-04-05 18:24:54 +0200

    Ajout du constructeur d'une grille.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,20 @@ public class Main {
 
     public static void main(String[] args)
     {
+        int[][] grille = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        };
+
+        Sudoku sudoku = new Sudoku(grille); // On initialise la grille
+        sudoku.afficheGrille();
 
     }
 }
--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,2 +1,21 @@
 public class Sudoku {
+    public static final int NB_LIGNES   = 9;
+    public static final int NB_COLONNES = 9;
+    public int[][] grille;
+
+
+    // Constructeur
+    public Sudoku(int[][] grille) {
+        this.grille = grille;
+    }
+
+    public void afficheGrille()
+    {
+        for (int i = 0; i < NB_LIGNES; i++) {
+            for (int j = 0; j < NB_COLONNES; j++) {
+                System.out.print(this.grille[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
