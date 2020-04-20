25b3ea85d7a65667a4b1eae9885e1dc8ffe5f6ce
TheoJugand <theosam@live.fr>
2020-04-20 14:50:20 +0000

    Correction de la méthode display() dans la classe Sudoku

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
@@ -23,7 +23,7 @@ public class Sudoku {
     public void display() {
         for(int i = 0; i < SIZE; i++){
             for(int j = 0; j < SIZE; j++){
-                System.out.println(" " + board[i][j]);
+                System.out.print(" " + board[i][j]);
             }
             System.out.println();
         }
