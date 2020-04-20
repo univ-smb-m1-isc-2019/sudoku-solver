6befb5a8c317551c2f922ba462ec75e8c49db6ac
VincentBROZZONI <vincent-brozzoni@outlook.fr>
2020-04-20 14:37:02 +0200

    Création du constructeur de la classe Sudoku

--- solver/src/main/java/Sudoku.java
+++ solver/src/main/java/Sudoku.java
@@ -1,2 +1,12 @@
 public class Sudoku {
+
+    private int[][] board;
+
+    /**
+     * Contructeur de la classe Sudoku
+     * @param board
+     */
+    public Sudoku(int[][] board){
+        this.board = board;
+    }
 }
