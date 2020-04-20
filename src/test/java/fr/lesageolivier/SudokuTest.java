d521968c12b8596cab7546455ed15fd3a8dda3c8
OlivierLESAGE <contact@lesageolivier.fr>
2020-04-20 16:03:29 +0200

    Ajout d'une claase test pour Sudoku

---
+++ src/test/java/fr/lesageolivier/SudokuTest.java
@@ -0,0 +1,31 @@
+package fr.lesageolivier;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SudokuTest {
+    public static final int[][] board = {
+            { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+            { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+            { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+            { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+            { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+            { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+            { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+    };
+
+    private Sudoku suodku;
+
+    @BeforeEach
+    public void setUp() {
+        this.suodku = new Sudoku(SudokuTest.board);
+    }
+
+    @Test
+    public void testCalculer() throws Exception {
+        assertTrue(true);
+    }
+
+}
