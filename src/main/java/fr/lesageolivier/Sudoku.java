30139d75cfd41f0577523fe1314ec5e4a9e52997
OlivierLESAGE <contact@lesageolivier.fr>
2020-04-20 15:10:37 +0200

    Ajout d'une méthode permettant d'afficher la grille

--- src/main/java/fr/lesageolivier/Sudoku.java
+++ src/main/java/fr/lesageolivier/Sudoku.java
@@ -1,12 +1,31 @@
 package fr.lesageolivier;
 
+import java.awt.*;
+
 public class Sudoku {
     /**
      * Tableau contenant le Sudoku
      */
     private int[][] board;
 
+    /**
+     * Constructeur
+     *
+     * @param board Tableau contenant les valeur de la grille de jeu
+     */
     public Sudoku(int [][] board) {
         this.board = board;
+    }
+
+    /**
+     * Méthode permettant d'afficher la grille
+     */
+    public void show() {
+        for (int[] line: this.board) {
+            for (int cell: line) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
     }
 }
