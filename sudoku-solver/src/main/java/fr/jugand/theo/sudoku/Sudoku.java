0a2e8de2a1da6c898f3e4d3361e2a902b2a3a983
TheoJugand <theosam@live.fr>
2020-04-20 14:48:24 +0000

    Ajout d'une méthode permettant d'afficher la grille dans la classe Sudoku

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
@@ -21,5 +21,12 @@ public class Sudoku {
     }
 
     public void display() {
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                System.out.println(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
