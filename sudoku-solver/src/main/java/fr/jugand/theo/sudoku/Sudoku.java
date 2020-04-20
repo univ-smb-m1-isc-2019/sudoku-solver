5b4489b637052b0e856f6dea1115aec2e71246a2
TheoJugand <theosam@live.fr>
2020-04-20 14:36:56 +0000

    Création du constructeur de la classe Sudoku

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
@@ -1,4 +1,15 @@
 package fr.jugand.theo.sudoku;
 
+/**
+ * Classe représentant le sudoku
+ */
 public class Sudoku {
+
+    /**
+     * Grille du sudoku
+     */
+    private int[][] board;
+    public Sudoku(int[][] board){
+        this.board = board;
+    }
 }
