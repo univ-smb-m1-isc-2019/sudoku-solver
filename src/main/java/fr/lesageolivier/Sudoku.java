e99e7fcfecabb8f1cfa9781146860f01fb1e4b26
OlivierLESAGE <contact@lesageolivier.fr>
2020-04-20 15:22:37 +0200

    Ajout d'une méthode IsInRow permettant de savoir si un nombre est dans une ligne

--- src/main/java/fr/lesageolivier/Sudoku.java
+++ src/main/java/fr/lesageolivier/Sudoku.java
@@ -21,6 +21,21 @@ public class Sudoku {
     }
 
     /**
+     * Méthode permettant de savoir si un nombre est dans une ligne
+     *
+     * @param row Le numéro de la ligne
+     * @param number Le nombre dont on veut connaitre la présence
+     * @return true si le nombre est présent dans la ligne, false sinon
+     */
+    private boolean isInRow(int row, int number) {
+        for (int i = 0; i < SIZE; i++)
+            if (board[row][i] == number)
+                return true;
+
+        return false;
+    }
+
+    /**
      * Méthode permettant d'afficher la grille
      */
     public void show() {
