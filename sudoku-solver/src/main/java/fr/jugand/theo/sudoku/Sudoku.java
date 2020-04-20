45f9e23e0b15aefe42d9264d6d672c7910ecbc4d
TheoJugand <theosam@live.fr>
2020-04-20 15:36:12 +0000

    Ajout de commentaires à la méthode solve()

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Sudoku.java
@@ -83,31 +83,32 @@ public class Sudoku {
         return !isInRow(row, number)  &&  !isInCol(column, number)  &&  !isInContainer(row, column, number);
     }
 
+    /**
+     * Méthode permettant de vérifier si on peut résoudre la grille du sudoku
+     * @return Un booléen pour savoir si la grille est résolvable
+     */
     public boolean solve() {
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
-                // we search an empty cell
                 if (board[row][col] == EMPTY) {
-                    // we try possible numbers
                     for (int number = 1; number <= SIZE; number++) {
                         if (isPlaceable(row, col, number)) {
-                            // number ok. it respects sudoku constraints
                             board[row][col] = number;
 
-                            if (solve()) { // we start backtracking recursively
+                            if (solve()) {
                                 return true;
-                            } else { // if not a solution, we empty the cell and we continue
+                            } else {
                                 board[row][col] = EMPTY;
                             }
                         }
                     }
 
-                    return false; // we return false
+                    return false;
                 }
             }
         }
 
-        return true; // sudoku solved
+        return true;
     }
 
     /**
