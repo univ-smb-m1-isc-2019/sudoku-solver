e294764bdddae899cbe712ddc00bf3fb9a98b520
TheoJugand <theosam@live.fr>
2020-04-21 15:04:37 +0000

    Ajout des commentaires dans la classe Case

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Case.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Case.java
@@ -1,20 +1,38 @@
 package fr.jugand.theo.sudoku;
 
+/**
+ * Classe représentant une case
+ */
 public class Case {
+    /**
+     * Valeur de la case
+     */
     private int value;
 
     public Case(int value){
         this.value = value;
     }
 
+    /**
+     * Retourne  la valeur d'une case
+     * @return Valeur de la case
+     */
     public int getValue(){
         return this.value;
     }
 
+    /**
+     * Attribue une valeur à une case
+     * @param value Valeur à attribuer
+     */
     public void setValue(int value){
         this.value = value;
     }
 
+    /**
+     * Verifie si la case est égale à zero
+     * @return Un booléen pour savoir si la case est égale à 0
+     */
     public boolean isEmpty(){
         return this.value == 0;
     }
