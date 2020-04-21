5794dfffb27f14eb4b7841815cfe93324664353b
TheoJugand <theosam@live.fr>
2020-04-21 16:03:41 +0000

    Ajout d'un attibut listCell + constructeur dans la classe Container

--- sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Container.java
+++ sudoku-solver/src/main/java/fr/jugand/theo/sudoku/Container.java
@@ -1,4 +1,15 @@
 package fr.jugand.theo.sudoku;
 
+/**
+ * Classe représentant un groupe de case
+ */
 public class Container {
+    /**
+     * Groupe de case
+     */
+    public Cell[] listCell;
+
+    public Container(){
+        this.listCell = new Cell[Sudoku.SIZE];
+    }
 }
