0c192876adf1efa0a7042cf171e6140635fc57e4
TheoJugand <theosam@live.fr>
2020-04-21 14:21:33 +0000

    Ajout d'un test permettantt de savoir si la grille est complète (tous les éléments de la grille sont différents de 0) une fois résolue

--- sudoku-solver/src/test/java/fr/jugand/theo/sudoku/SudokuTest.java
+++ sudoku-solver/src/test/java/fr/jugand/theo/sudoku/SudokuTest.java
@@ -1,37 +1,37 @@
 package fr.jugand.theo.sudoku;
-import org.junit.Test;
-import org.junit.jupiter.api.*;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class SudokuTest {
+    public static final int[][] board = {
+            { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+            { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+            { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+            { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+            { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+            { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+            { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+    };
 
-    private Sudoku sudoku;
+    private Sudoku suodku;
+
     @BeforeEach
-    public void createAndSolvedGrid(){
-        int[][] board = {
-                {8, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 3, 6, 0, 0, 0, 0, 0},
-                {0, 7, 0, 0, 9, 0, 2, 0, 0},
-                {0, 5, 0, 0, 0, 7, 0, 0, 0},
-                {0, 0, 0, 0, 4, 5, 7, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 3, 0},
-                {0, 0, 1, 0, 0, 0, 0, 6, 8},
-                {0, 0, 8, 5, 0, 0, 0, 1, 0},
-                {0, 9, 0, 0, 0, 0, 4, 0, 0}
-        };
-        sudoku = new Sudoku(board);
-        sudoku.solve();
+    public void createAndResolve() {
+        this.suodku = new Sudoku(SudokuTest.board);
+        this.suodku.solve();
     }
 
     @Test
-    public void gridIsComplete(){
-        assertTrue(this.sudoku.isComplete());
+    public void testBoardIsComplete() {
+        assertTrue(this.suodku.isComplete());
     }
 
     @AfterEach
-    public void displaySudoku(){
-        this.sudoku.display();
+    public void displayGrid(){
+        this.suodku.display();
     }
+
 }
