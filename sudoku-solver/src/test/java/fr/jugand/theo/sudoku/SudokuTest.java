531957c62f7d27d78c216c0737e719c8c65ca994
TheoJugand <theosam@live.fr>
2020-04-22 10:42:09 +0000

    Ajout d'un test dans la classe SudokuTest permettant de savoir si tous les nombres de 1 à 9 sont pésents dans une colonne

--- sudoku-solver/src/test/java/fr/jugand/theo/sudoku/SudokuTest.java
+++ sudoku-solver/src/test/java/fr/jugand/theo/sudoku/SudokuTest.java
@@ -1,20 +1,24 @@
 package fr.jugand.theo.sudoku;
 
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SudokuTest {
     public static final int[][] board = {
-            { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
-            { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
-            { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
-            { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
-            { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
-            { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
-            { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
-            { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+            {8, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 3, 6, 0, 0, 0, 0, 0},
+            {0, 7, 0, 0, 9, 0, 2, 0, 0},
+            {0, 5, 0, 0, 0, 7, 0, 0, 0},
+            {0, 0, 0, 0, 4, 5, 7, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 3, 0},
+            {0, 0, 1, 0, 0, 0, 0, 6, 8},
+            {0, 0, 8, 5, 0, 0, 0, 1, 0},
+            {0, 9, 0, 0, 0, 0, 4, 0, 0}
     };
+
+    public static final int[] possibilities = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 
     private Sudoku sudoku;
 
@@ -29,8 +33,17 @@ public class SudokuTest {
         assertTrue(this.sudoku.isComplete());
     }
 
+    @Test
+    public void everyNumberFromOneToNineAreInColumnOnce() {
+        for (int i = 0; i < Sudoku.SIZE; i++) {
+            for (int j = 0; j < Sudoku.SIZE; j++) {
+                assertTrue(this.sudoku.columns[i].isInContainer(possibilities[j]));
+            }
+        }
+    }
+
     @AfterEach
-    public void displayGrid(){
+    public void displayGrid() {
         this.sudoku.display();
     }
 
