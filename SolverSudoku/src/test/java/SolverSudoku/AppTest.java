0706d2525d50282acb4c957c7182dd93523dcb87
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-04-25 23:13:51 +0200

    Mise en place du troisieme test

--- SolverSudoku/src/test/java/SolverSudoku/AppTest.java
+++ SolverSudoku/src/test/java/SolverSudoku/AppTest.java
@@ -56,5 +56,30 @@ public class AppTest
 
     }
 
+    /**
+     * Test 3.
+     * Test that solves a single region
+     */
+    @Test
+    public void solvesSingleRegion()
+    {
+        int[][] grid = {
+                { 0, 0, 0, 7, 1, 8, 9, 4, 3 },
+                { 0, 0, 0, 9, 2, 3, 7, 6, 5 },
+                { 0, 0, 0, 6, 4, 5, 8, 1, 2 },
+                { 9, 6, 5, 2, 3, 7, 4, 8, 1 },
+                { 3, 8, 7, 4, 5, 1, 2, 9, 6 },
+                { 4, 1, 2, 8, 9, 6, 5, 3, 7 },
+                { 8, 3, 1, 5, 7, 4, 6, 2, 9 },
+                { 2, 5, 4, 1, 6, 9, 3, 7, 8 },
+                { 6, 7, 9, 3, 8, 2, 1, 5, 4 }
+        };
+
+
+        Sudoku sudoku = new Sudoku(grid);
+        sudoku.solve();
+        assertTrue( sudoku.isResolvedCorrectly() );
+    }
+
   
 }
