eb33f196097305c5efa5361194d839fc3c41babc
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-04-25 23:30:51 +0200

    Creation de la classe Boite et ses methodes

---
+++ SolverSudoku/src/main/java/SolverSudoku/Boite.java
@@ -0,0 +1,122 @@
+package  SolverSudoku;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+    public int number;
+    public int line;
+    public int column;
+    public Region region;
+
+    boolean valid;
+    public List<Integer> possibleNumbers = new ArrayList<Integer>();
+
+    // BUILDER
+    public Box(int number, int line, int column, Region region){
+        this.number = number;
+        this.line = line;
+        this.column = column;
+        this.region = region;
+        if(this.number != 0)
+            this.valid = true;
+        else{
+            this.valid = false;
+            for(int i=1; i <= 9; i++){
+                this.possibleNumbers.add(i);
+            }
+        }
+
+    }
+
+    public Box(int number, int line, int column, Region region, List<Integer> possibleNumbers, boolean valid){
+        this.number = number;
+        this.line = line;
+        this.column = column;
+        this.region = region;
+        this.valid = valid;
+        this.possibleNumbers = possibleNumbers;
+    }
+
+    public int getSizePossibleNumbers(){
+        return this.possibleNumbers.size();
+    }
+
+    public int getNumberPossibleNumbers(int index){
+        return this.possibleNumbers.get(index);
+    }
+
+    public void changeBox(int newNumber){
+        this.number = newNumber;
+        this.valid = true;
+    }
+
+    public void updateNumber(){
+        if(this.possibleNumbers.size() == 1){
+            this.number = this.possibleNumbers.get(0);
+            this.possibleNumbers.clear();
+            this.valid = true;
+        }
+
+    }
+
+    public void updateListPossibleNumbers(Box[][] grid){
+        this.possibleNumbers.removeAll(this.areaList(grid));
+    }
+
+    public boolean sameListMembers(Box box){
+        return this.possibleNumbers.containsAll(box.possibleNumbers);
+    }
+
+    public void cleanPossibleNumbers(){
+        if(this.valid && this.possibleNumbers != null)
+            this.possibleNumbers.clear();
+    }
+
+
+    public boolean isInArea(int num, Box[][] grid){
+        if(this.areaList(grid).contains(num))
+            return true;
+        else
+            return false;
+    }
+
+    public List<Integer> areaList(Box[][] grid){
+        List<Integer> list = new ArrayList<Integer>();
+        list.addAll(areaLine(grid));
+        list.addAll(areaColumn(grid));
+        list.addAll(this.region.areaRegion(grid, this.line, this.column));
+
+        return list;
+    }
+
+    public List<Integer> areaLine(Box[][] grid){
+        List<Integer> listLine = new ArrayList<Integer>();
+        for(int i=0; i <= 8; i++){
+            if(this.column != i && grid[this.line][i].valid)
+                listLine.add(grid[this.line][i].number);
+        }
+        return listLine;
+    }
+
+    public List<Integer> areaColumn(Box[][] grid){
+        List<Integer> listColumn = new ArrayList<Integer>();
+        for(int i=0; i <= 8; i++){
+            if(this.line != i && grid[i][this.column].valid)
+                listColumn.add(grid[i][this.column].number);
+        }
+        return listColumn;
+    }
+
+    public void displays_possibleNumbers(){
+        if(this.possibleNumbers.isEmpty() || this.possibleNumbers == null){
+            System.out.print("");
+        }
+        else{
+            for(int element : this.possibleNumbers){
+                System.out.print(element);
+            }
+        }
+    }
+
+}
