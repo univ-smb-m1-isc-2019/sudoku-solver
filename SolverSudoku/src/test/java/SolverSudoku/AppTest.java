4d224b5bf4609c8c03ff3a638f38392816c5d628
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-04-25 23:12:03 +0200

    Mise en place du second test

--- SolverSudoku/src/test/java/SolverSudoku/AppTest.java
+++ SolverSudoku/src/test/java/SolverSudoku/AppTest.java
@@ -7,14 +7,54 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
-     * Rigorous Test :-)
+     * Test 1.
+     * Test if the grid is correct and valid.
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void gridIsValid()
     {
-        assertTrue( true );
+        int[][] grid = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        };
+
+        Sudoku sudoku = new Sudoku(grid);
+        assertTrue( sudoku.isValid() );
     }
+
+    /**
+     * Test 2.
+     * Test : check a full grid.
+     */
+    @Test
+    public void checkFullGrid()
+    {
+        int[][] grid = {
+                { 5, 2, 6, 7, 1, 8, 9, 4, 3 },
+                { 1, 4, 8, 9, 2, 3, 7, 6, 5 },
+                { 7, 9, 3, 6, 4, 5, 8, 1, 2 },
+                { 9, 6, 5, 2, 3, 7, 4, 8, 1 },
+                { 3, 8, 7, 4, 5, 1, 2, 9, 6 },
+                { 4, 1, 2, 8, 9, 6, 5, 3, 7 },
+                { 8, 3, 1, 5, 7, 4, 6, 2, 9 },
+                { 2, 5, 4, 1, 6, 9, 3, 7, 8 },
+                { 6, 7, 9, 3, 8, 2, 1, 5, 4 }
+        };
+
+        Sudoku sudoku = new Sudoku(grid);
+        assertTrue( sudoku.isResolvedCorrectly() );
+
+    }
+
+  
 }
