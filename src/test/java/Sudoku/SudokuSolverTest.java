317d59f1c4aecd0c5cd18e98c5d81687d4233827
Paul <paullopez2904@gmail.com>
2020-04-25 19:56:41 +0200

    Tests added to check if the values in Line/Column/Matrice are valid

--- src/test/java/Sudoku/SudokuSolverTest.java
+++ src/test/java/Sudoku/SudokuSolverTest.java
@@ -5,22 +5,73 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Unit test for simple App.
  */
 public class SudokuSolverTest{
 
+
+    public static final Integer[] allValidValuesTab = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+
+    private Integer[] getActualValue(ArrayList<Cell> cells){
+        Integer[] res = new Integer[9];
+
+        for(int i = 0; i < res.length; ++i){
+            res[i] = cells.get(i).getValue();
+        }
+
+        return res;
+    }
+
     @Test
     public void validColumn() {
+        int[][] board = Main.board;
+        Sudoku sudoku = new Sudoku(board);
+        sudoku.solver();
+
+        for(int i = 0; i < 9; i++){
+            Column col = sudoku.getBoard().getColumns(i);
+            ArrayList<Cell> cells = col.getColumn();
+            Integer[] columnValues = getActualValue(cells);
+            Arrays.sort(columnValues);
+
+            assertThat(columnValues).isEqualTo(allValidValuesTab);
+        }
     }
 
     @Test
     public void validLine() {
+        int[][] board = Main.board;
+        Sudoku sudoku = new Sudoku(board);
+        sudoku.solver();
+
+        for(int i = 0; i < 9; i++){
+            Line line = sudoku.getBoard().getLines(i);
+            ArrayList<Cell> cells = line.getLine();
+            Integer[] lineValues = getActualValue(cells);
+            Arrays.sort(lineValues);
+
+            assertThat(lineValues).isEqualTo(allValidValuesTab);
+        }
     }
 
     @Test
     public void validMatrice() {
+        int[][] board = Main.board;
+        Sudoku sudoku = new Sudoku(board);
+        sudoku.solver();
+
+        for(int i = 0; i < 9; i++){
+            Matrice mat = sudoku.getBoard().getMatrix(i);
+            ArrayList<Cell> cells = mat.getMatrix();
+            Integer[] matValues = getActualValue(cells);
+            Arrays.sort(matValues);
+
+            assertThat(matValues).isEqualTo(allValidValuesTab);
+        }
     }
 
 
