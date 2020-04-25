2195f2992cf80e74ef60bf700657b8f22fef6ca7
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-04-25 23:15:01 +0200

    Mise en place du quatrieme test

--- SolverSudoku/src/test/java/SolverSudoku/AppTest.java
+++ SolverSudoku/src/test/java/SolverSudoku/AppTest.java
@@ -81,5 +81,30 @@ public class AppTest
         assertTrue( sudoku.isResolvedCorrectly() );
     }
 
-  
+    /**
+     * Test 4.
+     * Test that solves a easy grid
+     */
+    @Test
+    public void solvesEasyGrid()
+    {
+        int[][] grid = {
+                { 7, 9, 4, 0, 8, 6, 3, 1, 0 },
+                { 1, 0, 2, 0, 7, 3, 5, 8, 9 },
+                { 0, 5, 0, 0, 9, 2, 0, 0, 0 },
+                { 0, 0, 0, 7, 5, 0, 1, 2, 0 },
+                { 0, 0, 7, 3, 0, 0, 0, 9, 6 },
+                { 0, 4, 0, 0, 1, 0, 0, 0, 0 },
+                { 4, 3, 0, 0, 0, 0, 0, 5, 1 },
+                { 0, 0, 0, 9, 0, 0, 6, 0, 0 },
+                { 6, 0, 1, 0, 3, 0, 0, 0, 0 }
+        };
+
+
+        Sudoku sudoku = new Sudoku(grid);
+        sudoku.solve();
+        assertTrue( sudoku.isResolvedCorrectly() );
+    }
+
+   
 }
