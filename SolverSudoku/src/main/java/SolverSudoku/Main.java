f8cdb57cb0e15d53bd3825a85015408a6b1cdaed
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-04-26 02:51:23 +0200

    Creation de la classe Main

---
+++ SolverSudoku/src/main/java/SolverSudoku/Main.java
@@ -0,0 +1,29 @@
+package SolverSudoku;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        // Sudoku grid
+        int[][] grid = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        };
+
+        Sudoku sudoku = new Sudoku(grid);
+        sudoku.solve();
+    }
+
+}
